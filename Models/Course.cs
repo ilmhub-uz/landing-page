b26915fb7fbe08655cd575ffd1c867e44a6f2758
shahbozkhurramov <shahbozkhurramov@gmail.com>
2022-02-08 00:41:15 +0500

    removed base href

--- Models/Course.cs
+++ Models/Course.cs
@@ -8,11 +8,23 @@ public class Course
     
     public string Tag { get; set; }
     
-    public Double Score { get; set; }
+    public float Score { get; set; }
     
     public int CostInUzs { get; set; }
     
-    public int DurationInMonth { get; set; }
+    public float CourseDuration { get; set; }
+
+    public float ClassDuration { get; set; }
+    
+    public string InstructorName { get; set; }
 
     public string BannerId { get; set; }
+
+    public List<string> Themes { get; set; }
+
+    public string CourseLevel { get; set; }
+    
+    public string Deadline { get; set; }
+    
+    
 }
---
+++ Models/Instructor.cs
@@ -0,0 +1,24 @@
+namespace landing.Models;
+
+public class Instructor
+{
+    public string InstructorName { get; set; }
+    
+    public string AboutInstructor { get; set; }
+    
+    public float Score { get; set; }
+        
+    public string MentorWorkPlace { get; set; }
+
+    public string InstagramUrl { get; set; }
+
+    public string FacebookUrl { get; set; }
+
+    public string LinkedInUrl { get; set; }
+
+    public string TelegramUrl { get; set; }
+
+    public string TwitterUrl { get; set; }
+
+    public string StudentCount { get; set; }
+}
