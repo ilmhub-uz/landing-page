69cb49b95825a6ac0de097a15adda267db662316
shahbozkhurramov <shahbozkhurramov@gmail.com>
2022-02-10 19:38:51 +0500

    connection null exception fixed

--- wwwroot/assets/js/functions.js
+++ wwwroot/assets/js/functions.js
@@ -11,12 +11,12 @@ function Send_Data(url, formtype){
     var form = document.forms[formtype]
     fetch(url, { method: 'POST', body: new FormData(form)})
     .then(response => $("#form_alerts").html("<div class='alert alert-success'>Xabar muvaffaqiyatli yetkazildi</div>"))
-    .catch(error => $("#form_alerts").html("<div class='alert alert-danger'>Xabarni yetkazishda xatolik yuz berdi! Iltimos qaytadan urinib ko'ring.</div>"))
+    .catch(error => $("#form_alerts").html("<div class='alert alert-danger'>Xabarni yetkazishda xatolik yuz berdi! Iltimos qaytadan urinib ko'ring.</div>"));
     
     form.addEventListener('submit', e => {
-        e.preventDefault()
+        e.preventDefault();
     });
-    
+    console.log(jQuery("#form_alerts").val());
     jQuery.ajax({ 
         success: function(response) {
             document.forms[formtype].reset();
