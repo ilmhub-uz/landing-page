eb6d34fb5ea4767dfc27dccee9e447ef76dd6754
shahbozkhurramov <shahbozkhurramov@gmail.com>
2022-02-16 23:40:52 +0500

    button disabled

--- wwwroot/assets/js/functions.js
+++ wwwroot/assets/js/functions.js
@@ -11,15 +11,17 @@ function Send_Data(url, formtype){
     if(formtype=="contact-form" && jQuery("#message").val() == ""){
       return false;
     }
-    var form = document.forms[formtype]
+    document.getElementById("SendButton").disabled = true;
+    const form = document.forms[formtype]
     fetch(url, { method: 'POST', body: new FormData(form)})
     .then(response => $("#form_alerts").html("<div class='alert alert-success'>Xabar muvaffaqiyatli yetkazildi</div>"))
-    .catch(error => $("#form_alerts").html("<div class='alert alert-danger'>Xabarni yetkazishda xatolik yuz berdi! Iltimos qaytadan urinib ko'ring.</div>"));
+    .catch(error => $("#form_alerts").html("<div class='alert alert-danger'>Xabarni yetkazishda xatolik yuz berdi! Iltimos qaytadan urinib ko'ring.</div>"))
+    .finally(response => document.getElementById("SendButton").disabled = false);
     
     form.addEventListener('submit', e => {
       e.preventDefault();
     });
-    // document.forms[formtype].reset();
+    console.log(form)
     jQuery.ajax({ 
         success: function(response) {
             jQuery("#"+formtype)[0].reset();
